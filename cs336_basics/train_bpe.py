942ca20ac118cd1f4b2eb03dd01bcaf64c3fe5d7
Yang Liu <yangliu9921@gmail.com>
2025-05-21 06:27:18 +0000

    add multiprocessing in pretokenization

--- cs336_basics/train_bpe.py
+++ cs336_basics/train_bpe.py
@@ -1,16 +1,87 @@
 import regex as re
 import time
+import os
+from typing import BinaryIO
+import multiprocessing
+
+def find_chunk_boundaries(
+    file: BinaryIO, 
+    desired_num_chunks: int, 
+    split_special_token: bytes
+) -> list[int]:
+    """
+    Chunk the file into parts that can be counted independently.
+    May return fewer chunks if the boundaries end up overlapping.
+    """
+    assert isinstance(split_special_token, bytes), (
+        "Must represent special token as a bytestring"
+    )
+
+    # Get total file size in bytes
+    file.seek(0, os.SEEK_END)
+    file_size = file.tell()
+    file.seek(0)
+
+    chunk_size = file_size // desired_num_chunks
+
+    # Initial guesses for chunk boundary locations, uniformly spaced
+    # Chunks start on previous index, don't include last index
+    chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
+    chunk_boundaries[-1] = file_size
+
+    mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
+
+    for bi in range(1, len(chunk_boundaries) - 1):
+        initial_position = chunk_boundaries[bi]
+        file.seek(initial_position)  # Start at boundary guess
+        while True:
+            mini_chunk = file.read(mini_chunk_size)  # Read a mini chunk
+
+            # If EOF, this boundary should be at the end of the file
+            if mini_chunk == b"":
+                chunk_boundaries[bi] = file_size
+                break
+
+            # Find the special token in the mini chunk
+            found_at = mini_chunk.find(split_special_token)
+            if found_at != -1:
+                chunk_boundaries[bi] = initial_position + found_at
+                break
+            initial_position += mini_chunk_size
+
+    # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
+    return sorted(set(chunk_boundaries))
+
+
+def pretokenize_chunk(start, end, input_path, special_tokens):
+    freq = {}
+    PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+    with open(input_path, "rb") as f:
+        f.seek(start)
+        chunk = f.read(end - start).decode("utf-8", errors="ignore")
+
+        # Run pre-tokenization on your chunk and store the counts for each pre-token
+        text_splits = re.split("|".join(special_tokens), chunk)
+        for text_i in text_splits:
+            # if text_i == "|":
+            #     continue
+            for s in re.finditer(PAT, text_i):
+                s = s.group(0)
+                s = tuple([bytes([i]) for i in s.encode("utf-8")])
+                freq[s] = freq.get(s, 0) + 1
+    return freq
 
 def train_bpe(
     input_path: str, 
     vocab_size: int,
-    special_tokens: list[str]
+    special_tokens: list[str], 
+    num_processes: int=8
 ):
 
     ### read text from input_path
-    with open(input_path, 'r') as file:
-        text = file.read()
-
+    # with open(input_path, 'r') as file:
+    #     text = file.read()
+    
 
     ### initialize vocabulary
     vocab = {i: x.encode("utf-8") for i, x in enumerate(special_tokens)}
@@ -20,18 +91,18 @@ def train_bpe(
 
 
     ### pre tokenization
-    # splits = text.replace("\n", " ").split(" ")
-    PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+    #### multiprocessing
+    with open(input_path, "rb") as f:
+        boundaries = find_chunk_boundaries(
+            f, num_processes*100, "<|endoftext|>".encode("utf-8"))
+
+    pool = multiprocessing.Pool(num_processes)
+    processes = [pool.apply_async(pretokenize_chunk, args=(start, end, input_path, special_tokens)) for start, end in zip(boundaries[:-1], boundaries[1:])]
     freq = {}
-    text_splits = re.split("|".join(special_tokens), text)
-    for text_i in text_splits:
-        # if text_i == "|":
-        #     continue
-        for s in re.finditer(PAT, text_i):
-            s = s.group(0)
-            s = tuple([bytes([i]) for i in s.encode("utf-8")])
-            freq[s] = freq.get(s, 0) + 1
-    # print(freq)
+    for p in processes:
+        freq_chunk = p.get()
+        for s in freq_chunk:
+            freq[s] = freq.get(s, 0) + freq_chunk[s]
 
 
     ### count frequency of every successive pair of bytes
@@ -80,26 +151,7 @@ def train_bpe(
             s_new += s[indices[-1]+2:]
             freq[s_new] = freq.get(s_new, 0) + freq[s]
             # print(s, s_new)
-
             # print(s, indices, merged_pair)
-
-            # ### update pair-freq dict
-            # for j in indices:
-
-            #     # the pair with its prefix
-            #     if j >= 1:
-            #         old_pair = (s[j-1], s[j])
-            #         new_pair = (s[j-1], s[j]+s[j+1])
-            #         cnt_bytes_pairs[new_pair] = cnt_bytes_pairs.get(new_pair, 0) + freq[s]
-            #         cnt_bytes_pairs[old_pair] -= freq[s]
-
-            #     # the pair with its postfix
-            #     if j+2 < len(s):
-            #         old_pair = (s[j+1], s[j+2])
-            #         new_pair = (s[j]+s[j+1], s[j+2])
-            #         cnt_bytes_pairs[new_pair] = cnt_bytes_pairs.get(new_pair, 0) + freq[s]
-            #         cnt_bytes_pairs[old_pair] -= freq[s]
-
             
             ### update pair-appearance/freq dict
             # delete all pairs in `s`
@@ -130,7 +182,8 @@ def train_bpe(
 
 if __name__ == "__main__":
 
-    input_path = "/home/ec2-user/assignment1-basics/tests/fixtures/corpus.en"
+    input_path = "/home/ec2-user/data/TinyStoriesV2-GPT4-valid.txt"
+    # "/home/ec2-user/assignment1-basics/tests/fixtures/corpus.en"
     # "/home/ec2-user/data/simple_text.txt"
     vocab_size = 500
     special_tokens = ["<|endoftext|>"]
