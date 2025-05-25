8335d47938d0c8031df4a229e9366d3a5641e049
Yang Liu <yangliu9921@gmail.com>
2025-05-25 04:15:24 +0000

    add RMSNorm

--- tests/adapters.py
+++ tests/adapters.py
@@ -386,7 +386,9 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    layer_norm = model.rmsnorm(d_model=d_model, eps=eps)
+    layer_norm.load_state_dict({"weight": weights})
+    return layer_norm(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
