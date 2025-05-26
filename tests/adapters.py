82f4f45b5eed3b5b4b95f0616f249e2172c9ed7c
Yang Liu <yangliu9921@gmail.com>
2025-05-26 05:12:22 +0000

    add scaled dot product attention

--- tests/adapters.py
+++ tests/adapters.py
@@ -114,7 +114,7 @@ def run_scaled_dot_product_attention(
     Returns:
         Float[Tensor, " ... queries d_v"]: Output of SDPA
     """
-    raise NotImplementedError
+    return model.Attention(queries=Q, keys=K, values=V, mask=mask)
 
 
 def run_multihead_self_attention(
