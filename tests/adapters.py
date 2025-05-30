dd817718426df432ea487b76b065463e3fa7d3e4
Yang Liu <yangliu9921@gmail.com>
2025-05-30 05:54:39 +0000

    add lr scheduler

--- tests/adapters.py
+++ tests/adapters.py
@@ -530,7 +530,13 @@ def run_get_lr_cosine_schedule(
     Returns:
         Learning rate at the given iteration under the specified schedule.
     """
-    raise NotImplementedError
+    return optimizer.learning_rate_cosine_schedule(
+        t = it, 
+        lr_max = max_learning_rate, 
+        lr_min = min_learning_rate, 
+        warmup_iters = warmup_iters, 
+        cosine_cycle_iters = cosine_cycle_iters
+    )
 
 
 def run_save_checkpoint(
